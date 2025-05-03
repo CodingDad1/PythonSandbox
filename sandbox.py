1c34b2c996b270737fab30855ea21a3b7c251ef9
Nicholas Rodriguez <nickjrodriguez02@gmail.com>
2025-05-03 01:04:51 -0500

    Added Monster Class

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,17 @@
 import time
 import pyfiglet
 ascii_banner = pyfiglet.figlet_format("Mystvale")
+#Monster Class
+class Monster():
+    def __init__(self, name, health, enemytype, armorlevel, family):
+        self.name = name
+        self.health = health
+        self.enemytype = enemytype
+        self.armorlevel = armorlevel
+        self.family = family
+#All Normal EnemyTypes will have a MINIMUM roll of 5
+goblin = Monster("Goblin", 10, "Normal", 5, "Humanoid")
+zombie = Monster("Zombie", 5, "Normal", 5, "Undead")
 
 def show_intro():
     print(ascii_banner)
@@ -9,7 +20,7 @@ def show_intro():
     getUsersName()
 def getUsersName():
 #Get Users Name
-    name = input("What is your name warrior: ")
+    name = input("What is your name Stranger?: ")
     print("Are you sure your name is " + name + "?")
     isNameCorrect = False
 show_intro()
