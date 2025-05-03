034d7e356c3e5a3f248250e3b53ccbced2386732
Nicholas Rodriguez <nickjrodriguez02@gmail.com>
2025-05-03 00:19:41 -0500

    Added Intro

--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,15 @@
-print("hi")
-print("AnotherLine")
+import time
+import pyfiglet
+ascii_banner = pyfiglet.figlet_format("Mystvale")
+
+def show_intro():
+    print(ascii_banner)
+    print("New Game Dev, Please Enjoy")
+    time.sleep(5)
+    getUsersName()
+def getUsersName():
+#Get Users Name
+    name = input("What is your name warrior: ")
+    print("Are you sure your name is " + name + "?")
+    isNameCorrect = False
+show_intro()
